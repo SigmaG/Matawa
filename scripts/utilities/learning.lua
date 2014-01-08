6046e93d1fa449301c46068ec1879e31f7cdc4c7
Josh Marantz <marantzj@gmail.com>
2014-01-08 15:40:22 -0500

    Learning 2
    Updated learning script to handle token item that speeds learning
    Fixed invisible error when the learning queue runs out
    Added alias and triggers for learning

--- scripts/utilities/learning.lua
+++ scripts/utilities/learning.lua
@@ -7,8 +7,13 @@ function mtw.learn_start(lessons, skill, teacher)
 	mtw.learn_next()
 end
 
-function mtw.learn_next()
-	send("learn " .. math.min(mtw.learning.lessons, 15) .. " " .. mtw.learning.skill .. " from " .. mtw.learning.teacher,false)
+function mtw.learn_next(token)
+	if not mtw.learning then return end
+	if token then
+		mtw.set_balance("learn " .. math.min(mtw.learning.lessons, 15) .. " " .. mtw.learning.skill .. " from " .. mtw.learning.teacher)
+	else
+		send("learn " .. math.min(mtw.learning.lessons, 15) .. " " .. mtw.learning.skill .. " from " .. mtw.learning.teacher,false)
+	end
 	
 	mtw.learning.lessons = math.max( mtw.learning.lessons - 15, 0)
 	if mtw.learning.lessons == 0 then mtw.learning = nil end
