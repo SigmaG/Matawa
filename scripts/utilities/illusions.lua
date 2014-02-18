af654103e64cdb4c7776d256cbfda90673928f49
SigmaG <sigma_github@melix.net>
2014-02-18 07:23:34 +0100

    Additional illusion security, prevents multiple triggers from matching them. Though, the prompt is now considered as 'part' of the illusion.

--- scripts/utilities/illusions.lua
+++ scripts/utilities/illusions.lua
@@ -4,7 +4,7 @@
 
 function mtw.check_illusion(fct,matches,multimatches)
 if mtw.see_illusion then
- mtw.see_illusion = false
+ tempTimer(0,[[mtw.see_illusion = false]])
  cecho("\n<pink>Illusion checked!")
 else
  fct(matches,multimatches)
