SigmaG/Matawa

commit 700b3ed0d82326841c33952ebeb8f9594ef617a6

Added a generic area to bash some mobs - wyrms, pumpkins - everywhere

scripts/bashing/table.lua
@@ -3,6 +3,7 @@
 mtw_struct.set_metatable_custom("areas")
 
 mtw.default.areas = {
+["generic"] = {level = '", krondor = "none", targets = {"an ethereal mana wyrm", "a monstrous pumpkin", "a huge pumpkin", "a big pumpkin", "a mature pumpkin", "a medium pumpkin", "a small pumpkin", "a little pumpkin", "a tiny pumpkin", "a time%-displaced glamredhel archer", "a time%-displaced glamredhel warrior"}, items = {"a mote of glowing%, blue mana"}},
 ["MoteHunt"] = {level = "Magicians", krondor = "none", targets = {"an ethereal mana wyrm"}, items = {"a mote of glowing%, blue mana"}},
 ["pumpkins"] = {level = "LOL", krondor = "none", targets = {"a monstrous pumpkin", "a huge pumpkin", "a big pumpkin", "a mature pumpkin", "a medium pumpkin", "a small pumpkin", "a little pumpkin", "a tiny pumpkin"}},
 ["Krondor"] = {level = "Ratting", krondor = "none", prime = {"urban", "estate"}, env = {"garden", "ruins"}, targets = {"a black rat","a rat", "an old rat", "a baby rat", "a young rat"}},

scripts/bashing/load.lua
@@ -10,6 +10,9 @@ function mtw.load_targets(area)
    mtw.area_found = true
    mtw.bashing.target_type = "long"
    mtw.possible_targets = mtw.areas[area].targets
+   for i,v in ipairs(mtw.areas.generic.targets) do
+    table.append(mtw.possible_targets, v)
+   end
    echo("\n Level Range: "..mtw.areas[area].level)
    cecho("\n<green>Possible Targets:")
    for h in ipairs(mtw.possible_targets) do
@@ -17,6 +20,13 @@ function mtw.load_targets(area)
    end
    if mtw.areas[area].items then
     mtw.possible_items = mtw.areas[area].items
+   else
+    mtw.possible_items = {}
+   end 
+   for i,v in ipairs(mtw.areas.generic.items) do
+    table.append(mtw.possible_items, v)
+   end
+   if #mtw.possible_items > 0 then
     cecho("\n<green>Gathering Items:")
     for k in ipairs(mtw.possible_items) do
      echo("\n "..mtw.possible_items[k])

commit 62e04a7caf9749e25805319a0d7eb354bba013f9

Fixes error introduced in commit 700b3ed0

scripts/bashing/table.lua
@@ -3,7 +3,7 @@
 mtw_struct.set_metatable_custom("areas")
 
 mtw.default.areas = {
-["generic"] = {level = '", krondor = "none", targets = {"an ethereal mana wyrm", "a monstrous pumpkin", "a huge pumpkin", "a big pumpkin", "a mature pumpkin", "a medium pumpkin", "a small pumpkin", "a little pumpkin", "a tiny pumpkin", "a time%-displaced glamredhel archer", "a time%-displaced glamredhel warrior"}, items = {"a mote of glowing%, blue mana"}},
+["generic"] = {level = "", krondor = "none", targets = {"an ethereal mana wyrm", "a monstrous pumpkin", "a huge pumpkin", "a big pumpkin", "a mature pumpkin", "a medium pumpkin", "a small pumpkin", "a little pumpkin", "a tiny pumpkin", "a time%-displaced glamredhel archer", "a time%-displaced glamredhel warrior"}, items = {"a mote of glowing%, blue mana"}},
 ["MoteHunt"] = {level = "Magicians", krondor = "none", targets = {"an ethereal mana wyrm"}, items = {"a mote of glowing%, blue mana"}},
 ["pumpkins"] = {level = "LOL", krondor = "none", targets = {"a monstrous pumpkin", "a huge pumpkin", "a big pumpkin", "a mature pumpkin", "a medium pumpkin", "a small pumpkin", "a little pumpkin", "a tiny pumpkin"}},
 ["Krondor"] = {level = "Ratting", krondor = "none", prime = {"urban", "estate"}, env = {"garden", "ruins"}, targets = {"a black rat","a rat", "an old rat", "a baby rat", "a young rat"}},