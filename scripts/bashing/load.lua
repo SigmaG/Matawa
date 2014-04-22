59df24743e882805562def366004226d5bbbd5c5
SigmaG <sigma_github@melix.net>
2014-04-22 13:14:07 +0200

    Copy the values of possible targets in an area instead of reusing the table

--- scripts/bashing/load.lua
+++ scripts/bashing/load.lua
@@ -9,7 +9,9 @@ function mtw.load_targets(area)
  if mtw.areas[area] then
    mtw.area_found = true
    mtw.bashing.target_type = "long"
-   mtw.possible_targets = mtw.areas[area].targets
+   for i,v in ipairs(mtw.areas[area].targets) do
+    table.insert(mtw.possible_targets, v)
+   end
    for i,v in ipairs(mtw.areas.generic.targets) do
     table.insert(mtw.possible_targets, v)
    end
@@ -18,10 +20,11 @@ function mtw.load_targets(area)
    for h in ipairs(mtw.possible_targets) do
     echo("\n "..mtw.possible_targets[h])
    end
+   mtw.possible_items = {}
    if mtw.areas[area].items then
-    mtw.possible_items = mtw.areas[area].items
-   else
-    mtw.possible_items = {}
+    for i,v in ipairs(mtw.areas[area].items) do
+     table.insert(mtw.possible_items, v)
+    end
    end 
    for i,v in ipairs(mtw.areas.generic.items) do
     table.insert(mtw.possible_items, v)
