SigmaG/Matawa

commit 264926b98d4fb5d4e5999dd903b619da2bf060c5

Added sunspear defense

scripts/healing/defenses.lua
@@ -7,6 +7,7 @@ mtw.defenses = {
 def_myrrh = {defense = "learning speed", type = "normal", effect = "Shortens learning telegraph", balance = {balanceneed = "vout myrrh;eat myrrh"}, state = "down", needit = false, timer = createStopWatch()},
 def_highcastle = {defense = "", type = "normal", effect = "10% damage increase vs mobs", balance = {}, state = "down", needit = false, timer = createStopWatch()},
 def_veilgarden = {defense = "", type = "normal", effect = "Extends duration of defenses", skill = "veilgarden", balance = {}, state = "down", needit = false, timer = createStopWatch()},
+def_sunspear = {defense = "", type = "normal, effect = "Endurance regen", skill = "sunspear", balance = {}, state = "down", needit = false", timer = createStopWatch()},
 --Vigilance
 def_notice = {defense = "increased notice", type = "endurance", effect = "Checks against stealth actions", skill = "Notice", balance = {equilibriumtake= "notice"}, state = "down", needit = false, timer = createStopWatch()},
 def_skywatch = {defense = "skywatch", type = "normal", effect = "Detect rooftop movement", skill = "Skywatch", balance = {equilibriumtake= "skywatch on"}, state = "down", needit = false, timer = createStopWatch()},

scripts/triggers.lua
@@ -3414,6 +3414,10 @@ function mtw.trigger_579(matches,multimatches)
 mtw.def_def("veilgarden")
 end
 
+function mtw.trigger_579b(matches,multimatches)
+mtw.def_def("sunspear")
+end
+
 function mtw.trigger_580(matches,multimatches)
 mtw.def_def("berserking")
 end

triggers.xml
@@ -15497,6 +15497,26 @@
                                 </regexCodePropertyList>
                             </Trigger>
                             <Trigger isActive="yes" isFolder="no" isTempTrigger="no" isMultiline="no" isPerlSlashGOption="no" isColorizerTrigger="no" isFilterTrigger="no" isSoundTrigger="no" isColorTrigger="no" isColorTriggerFg="no" isColorTriggerBg="no">
+                                <name>sunspear</name>
+                                <script>mtw.check_illusion(mtw.trigger_579b,matches,multimatches)</script>
+                                <triggerType>0</triggerType>
+                                <conditonLineDelta>99</conditonLineDelta>
+                                <mStayOpen>0</mStayOpen>
+                                <mCommand></mCommand>
+                                <packageName></packageName>
+                                <mFgColor>#ff0000</mFgColor>
+                                <mBgColor>#ffff00</mBgColor>
+                                <mSoundFile></mSoundFile>
+                                <colorTriggerFgColor>#000000</colorTriggerFgColor>
+                                <colorTriggerBgColor>#000000</colorTriggerBgColor>
+                                <regexCodeList>
+                                    <string>You are regenerating endurance due to control of Sunspear.</string>
+                                </regexCodeList>
+                                <regexCodePropertyList>
+                                    <integer>3</integer>
+                                </regexCodePropertyList>
+                            </Trigger>
+                            <Trigger isActive="yes" isFolder="no" isTempTrigger="no" isMultiline="no" isPerlSlashGOption="no" isColorizerTrigger="no" isFilterTrigger="no" isSoundTrigger="no" isColorTrigger="no" isColorTriggerFg="no" isColorTriggerBg="no">
                                 <name>berserking def</name>
                                 <script>mtw.check_illusion(mtw.trigger_580,matches,multimatches)</script>
                                 <triggerType>0</triggerType>

commit e745a1d30ed4ce14270ba59d3387e8eceb2dd35a

Fixes error introduced in commit 264926b9

scripts/healing/defenses.lua
@@ -7,7 +7,7 @@ mtw.defenses = {
 def_myrrh = {defense = "learning speed", type = "normal", effect = "Shortens learning telegraph", balance = {balanceneed = "vout myrrh;eat myrrh"}, state = "down", needit = false, timer = createStopWatch()},
 def_highcastle = {defense = "", type = "normal", effect = "10% damage increase vs mobs", balance = {}, state = "down", needit = false, timer = createStopWatch()},
 def_veilgarden = {defense = "", type = "normal", effect = "Extends duration of defenses", skill = "veilgarden", balance = {}, state = "down", needit = false, timer = createStopWatch()},
-def_sunspear = {defense = "", type = "normal, effect = "Endurance regen", skill = "sunspear", balance = {}, state = "down", needit = false", timer = createStopWatch()},
+def_sunspear = {defense = "", type = "normal", effect = "Endurance regen", skill = "sunspear", balance = {}, state = "down", needit = false, timer = createStopWatch()},
 --Vigilance
 def_notice = {defense = "increased notice", type = "endurance", effect = "Checks against stealth actions", skill = "Notice", balance = {equilibriumtake= "notice"}, state = "down", needit = false, timer = createStopWatch()},
 def_skywatch = {defense = "skywatch", type = "normal", effect = "Detect rooftop movement", skill = "Skywatch", balance = {equilibriumtake= "skywatch on"}, state = "down", needit = false, timer = createStopWatch()},