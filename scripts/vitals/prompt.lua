d29ff846ff5ae00e4edd4996b375fabce5bc7661
JCalvinL <lkc3215@gmail.com>
2014-06-13 07:05:05 -0700

    added berserking to prompt
    player can now toggle berserk and it will send berserk when player is in
    combat.

--- scripts/vitals/prompt.lua
+++ scripts/vitals/prompt.lua
@@ -67,6 +67,16 @@ function mtw.prompt_function()
   end
  end
 
+ if mtw.status.combat and mtw.not_aff("timewarp") and mtw.not_aff("divine_censure") then
+  if mtw.toggles.berserking and mtw.balance.balance and not mtw.waiting.balance and mtw.not_aff("prone") and mtw.not_entangled() then
+   if mtw.my.class == "soldier" and table.contains(mtw.skills, "Berserking") and mtw.defenses.def_berserking.state ~= "deffed" and not berserk_sent then
+    mtw.send("berserk")
+    berserk_sent = true
+    tempTimer(mtw.delay(), [[berserk_sent = false]])
+   end
+  end
+ end
+
 --Reset important commands without ending lines
  for i in pairs(mtw.sent) do
   mtw.sent[i] = false
