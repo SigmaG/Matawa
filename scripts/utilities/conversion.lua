2c2d3cab62b593df1e2427ad05cb866f5d757892
SigmaG <sigma_github@melix.net>
2014-07-29 21:16:34 +0200

    Fixed the conversion module to put everything neatly in mtw table

--- scripts/utilities/conversion.lua
+++ scripts/utilities/conversion.lua
@@ -1,67 +1,68 @@
 -- MTW: functions about converting denizens to gods
 
-t_conversion = {"pontification","preaching","evangelizing","proselytizing"}
-conversion_size = 0
-conversion_first = false
-conversion_mob = ""
+mtw.conversion = {}
+mtw.conversion.t = {"pontification","preaching","evangelizing","proselytizing"}
+mtw.conversion.size = 0
+mtw.conversion.first = false
+mtw.conversion.mob = ""
 
 function mtw.reset_convert()
- conversion_mob = ""
- conversion_size = 0
- conversion_first = false
+ mtw.conversion.mob = ""
+ mtw.conversion.size = 0
+ mtw.conversion.first = false
  disableTrigger("random conversion answers")
  disableTrigger("attempt of conversion ended")
 end
 
 function mtw.attempt_conversion(denizen,str)
- conversion_mob = denizen
+ mtw.conversion.mob = denizen
  for i = 1,string.len(str) do
-  send("convert "..conversion_mob.." with "..t_conversion[tonumber(string.sub(str,i,i))])
+  send("convert "..mtw.conversion.mob.." with "..mtw.conversion.t[tonumber(string.sub(str,i,i))])
  end
 end
 
 
 function mtw.try_convert()
- for i=1,conversion_size do
-  send("convert "..conversion_mob.." with "..t_conversion[conversion_table[i]])
+ for i=1,mtw.conversion.size do
+  send("convert "..mtw.conversion.mob.." with "..mtw.conversion.t[mtw.conversion.table[i]])
  end
 end
 
 function mtw.conversion_answer()
- if conversion_first then
+ if mtw.conversion.first then
   mtw.first_try_convert()
  else
  end
 end
 
 function mtw.convert(denizen)
- conversion_mob = denizen
- conversion_size = 0
+ mtw.conversion.mob = denizen
+ mtw.conversion.size = 0
  enableTrigger("random conversion answers")
  enableTrigger("attempt of conversion ended")
- conversion_available = {}
+ mtw.conversion.available = {}
  mtw.first_try_convert()
- conversion_first = true
- conversion_get_numbers = false
+ mtw.conversion.first = true
+ mtw.conversion.get_numbers = false
 end
 
 function mtw.first_try_convert()
- send("convert "..conversion_mob.." with "..t_conversion[1])
- conversion_size = conversion_size + 1
+ send("convert "..mtw.conversion.mob.." with "..mtw.conversion.t[1])
+ mtw.conversion.size = mtw.conversion.size + 1
 end
 
 function mtw.find_numbers()
- for i = 1,conversion_size do
-  send("convert "..conversion_mob.." with "..t_conversion[conversion_current_test])
+ for i = 1,mtw.conversion.size do
+  send("convert "..mtw.conversion.mob.." with "..mtw.conversion.t[mtw.conversion.current_test])
  end
 end
 
 function mtw.start_actual_conversion()
- conversion_get_numbers = false
- mtw.conversion_all_generate()
- for k,v in pairs(conversion_all) do
-  conversion_table = v
-  conversion_all[k] = nil
+ mtw.conversion.get_numbers = false
+ mtw.conversion.all_generate()
+ for k,v in pairs(mtw.conversion.all) do
+  mtw.conversion.table = v
+  mtw.conversion.all[k] = nil
   break
  end
  tempTimer(0.2,[[mtw.try_convert()]])
@@ -70,18 +71,18 @@ end
 function mtw.conversion_attempt_result(matches)
  local good = tonumber(matches[2])
  local bad = tonumber(matches[3])
- if conversion_get_numbers then
+ if mtw.conversion.get_numbers then
   for i=1,good do
-   table.insert(conversion_available,conversion_current_test)
+   table.insert(mtw.conversion.available,mtw.conversion.current_test)
   end
-  if conversion_current_test < 4 then
-   if #conversion_available == conversion_size then
+  if mtw.conversion.current_test < 4 then
+   if #mtw.conversion.available == mtw.conversion.size then
     mtw.start_actual_conversion()
    else
-    conversion_current_test = conversion_current_test + 1
-    if conversion_current_test == 4 then
-     for i=1,conversion_size-#conversion_available do
-      table.insert(conversion_available,4)
+    mtw.conversion.current_test = mtw.conversion.current_test + 1
+    if mtw.conversion.current_test == 4 then
+     for i=1,mtw.conversion.size-#mtw.conversion.available do
+      table.insert(mtw.conversion.available,4)
      end
      mtw.start_actual_conversion()
     else
@@ -93,28 +94,28 @@ function mtw.conversion_attempt_result(matches)
   end
  return
  end
- if conversion_first then
+ if mtw.conversion.first then
   for i=1,good do
-   table.insert(conversion_available,1)
+   table.insert(mtw.conversion.available,1)
   end
-  conversion_first = false
+  mtw.conversion.first = false
   disableTrigger("random conversion answers")
-  conversion_get_numbers = true
-  conversion_current_test = 2
+  mtw.conversion.get_numbers = true
+  mtw.conversion.current_test = 2
   tempTimer(0.2,[[mtw.find_numbers()]])
   return
  end
- for k,v in pairs(conversion_all) do
-  conversion_table = v
-  conversion_all[k] = nil
+ for k,v in pairs(mtw.conversion.all) do
+  mtw.conversion.table = v
+  mtw.conversion.all[k] = nil
   break
  end
  tempTimer(0.2,[[mtw.try_convert()]])
 end
 
-function mtw.conversion_all_generate()
- conversion_all = {}
- mtw.permutation(conversion_available,conversion_size)
+function mtw.conversion.all_generate()
+ mtw.conversion.all = {}
+ mtw.permutation(mtw.conversion.available,mtw.conversion.size)
 end
 
 function mtw.permutation(a, n)
@@ -130,7 +131,7 @@ function mtw.permutation(a, n)
 			idx = idx + pow*new[i]
 			pow = pow * 10
 		end
-		conversion_all[idx] = new
+		mtw.conversion.all[idx] = new
 	else
 		for i = 1, n do
 			a[i], a[n] = a[n], a[i]
