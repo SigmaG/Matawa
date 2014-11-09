e0f822764ef9b4959a33ca904b1a2abcfa072082
SigmaG <sigma_github@melix.net>
2014-11-09 16:26:17 +0100

    initial GUI framework

---
+++ scripts/GUI/init.lua
@@ -0,0 +1,36 @@
+-- MTW: GUI functions
+
+mtw_struct.load_external("GUI/geyser")
+
+mtw_struct.load_external("GUI/config")
+
+if mtw.cgit.enabled then
+ mtw.gui = mtw.gui or {}
+
+ mtw_struct.load_external("GUI/utils")
+ mtw.gui.update_window_size()
+
+ mtw_struct.load_external("GUI/rootwin")
+
+ --here happens the magic!
+ mtw_struct.load_external("GUI/windows")
+
+ --we construct the three main containers (left/right/bottom) in there
+ mtw_struct.load_external("GUI/mainwindows")
+
+ --now we create the actual windows :)
+
+else
+ -- we just want to prevent any call to the GUI when it's not enabled
+ mtw.gui = {}
+ local mt = {
+  __call = function (...) return nil end,
+  __newindex = function (k,v,t)
+  end,
+  __index = function (k,v)
+   local z = {}
+   return setmetatable(z, mt)
+  end,
+ }
+ setmetatable(mtw.gui,mt)
+end
---
+++ scripts/GUI/rootwin.lua
@@ -0,0 +1,33 @@
+-- MTW GUI : create the framework to start using the rest.
+-- mtw.gui.root is an abstraction of the main window, mtw.gui.disabled is where the disabled windows go to die (or not).
+
+mtw.gui.root = mtw.gui.root or {
+ win = nil,
+ children = {},
+ add = function(self,win)
+ end,
+ remove = function(self,win)
+ end,
+ resize_chidren = function(self)
+  if mtw.cgui.chat_enabled or mtw.cgui.mapper_enabled then
+   setBorderLeft(mtw.gui.value_to_px("left_bar_width", mtw.gui.w))
+  end
+  if mtw.cgui.target_enabled or mtw.cgui.system_info_enabled or mtw.cgui.ih_enabled or mtw.cgui.game_info_enabled or mtw.cgui.aff_enabled or mtw.cgui.who_enabled then
+   setBorderRight(mtw.gui.value_to_px("right_bar_width", mtw.gui.w))
+  end
+  if mtw.cgui.gauges_enabled or mtw.cgui.location_enabled then
+   setBorderBottom(mtw.gui.value_to_px("bottom_bar_height", mtw.gui.h))
+  end
+  for _,v in pairs(self.children) do
+   v:reload()
+  end
+ end,
+}
+
+mtw.gui.disabled = mtw.gui.disabled or Geyser.Container:new({
+ name = "mtw_disabled",
+ x = 0,
+ y = 0,
+ width = 0,
+ height = 0,
+})
