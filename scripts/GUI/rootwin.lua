dc256752156db8934634b235dc332312538bd3a6
SigmaG <sigma_github@melix.net>
2014-11-10 12:02:40 +0100

    Let's try to not setBorders out of control

--- scripts/GUI/rootwin.lua
+++ scripts/GUI/rootwin.lua
@@ -14,16 +14,37 @@ mtw.gui.root = mtw.gui.root or {
   self:r()
   self:r()
  end,
+ borders = {top = 0, left = 0, right = 0, bottom = 0},
+ update_borders = function(self, top, left, right, bottom)
+  if top ~= self.borders.top then
+   self.borders.top = top
+   setBorderTop(top)
+  end
+  if left ~= self.borders.left then
+   self.borders.left = left
+   setBorderLeft(left)
+  end
+  if right ~= self.borders.right then
+   self.borders.right = right
+   setBorderRight(right)
+  end
+  if bottom ~= self.borders.bottom then
+   self.borders.bottom = bottom
+   setBorderBottom(bottom)
+  end
+ end,
  r = function(self)
+  local top, left, right, bottom = 0, 0, 0, 0
   if mtw.cgui.target_enabled or mtw.cgui.system_info_enabled or mtw.cgui.ih_enabled or mtw.cgui.game_info_enabled or mtw.cgui.aff_enabled or mtw.cgui.who_enabled then
-   setBorderLeft(mtw.gui.value_to_px("left_bar_width", mtw.gui.w))
+   left = mtw.gui.value_to_px("left_bar_width", mtw.gui.w)
   end
   if mtw.cgui.chat_enabled or mtw.cgui.mapper_enabled then
-   setBorderRight(mtw.gui.value_to_px("right_bar_width", mtw.gui.w))
+   right = mtw.gui.value_to_px("right_bar_width", mtw.gui.w)
   end
   if mtw.cgui.gauges_enabled or mtw.cgui.location_enabled then
-   setBorderBottom(mtw.gui.value_to_px("bottom_bar_height", mtw.gui.h))
+   bottom = mtw.gui.value_to_px("bottom_bar_height", mtw.gui.h)
   end
+  self:update_borders(top, left, right, bottom)
   for _,v in pairs(self.children) do
    v:reload()
   end
