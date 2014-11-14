ca437a600f160c3be387af2117c6148db78b5d47
SigmaG <sigma_github@melix.net>
2014-11-14 16:58:00 +0100

    Decorating bottom bars

--- scripts/GUI/bars.lua
+++ scripts/GUI/bars.lua
@@ -57,6 +57,90 @@ mtw.gui.status_bars.xp = mtw.gui.status_bars.xp or mtw.gui.new(Geyser.Gauge,
  mtw.cgui.xp_gauge_enabled,
  mtw.gui.status_b)
 
+mtw.gui.status_bars.hp.front:setStyleSheet([[background-color: QLinearGradient( x1: 1, y1: 0, x2: 0, y2: 1, stop: 0.0 #c40000, stop: 0.17 #0f0101, stop: 0.33 #170202, stop: 0.67 #0d0202, stop: 0.83 #140505, stop: 1 #260d0d);
+ border-width: 4px;
+ border-color: #005c28;
+ border-style: transparent;
+ border-radius: 2;
+ color: white;
+ padding: 0px;]])
+mtw.gui.status_bars.hp.back:setStyleSheet([[background-color: black;
+ border-width: 2px;
+ border-color: #005c28;
+ border-style: groove;
+ border-radius: 2;
+ color: white;
+ padding: 3px;]])
+
+mtw.gui.status_bars.pp.front:setStyleSheet([[background-color: QLinearGradient( x1: 1, y1: 0, x2: 0, y2: 1, stop: 0.0 #0066ff, stop: 0.24 #060614, stop: 0.50 #03030f, stop: 0.79 #04031c, stop: 1 #010014);
+ border-width: 4px;
+ border-color: #005c28;
+ border-style: transparent;
+ border-radius: 2;
+ color: white;
+ padding: 0px;]])
+mtw.gui.status_bars.pp.back:setStyleSheet([[background-color: black;
+ border-width: 2px;
+ border-color: #005c28;
+ border-style: groove;
+ border-radius: 2;
+ color: white;
+ padding: 3px;]])
+
+mtw.gui.status_bars.ep.front:setStyleSheet([[background-color: QLinearGradient( x1: 1, y1: 0, x2: 0, y2: 1, stop: 0.0 #e7fc00, stop: 0.24 #181a00, stop: 0.50 #090a01, stop: 0.79 #232600, stop: 1 #0e0f06);
+ border-width: 4px;
+ border-color: #005c28;
+ border-style: transparent;
+ border-radius: 2;
+ color: white;
+ padding: 0px;]])
+mtw.gui.status_bars.ep.back:setStyleSheet([[background-color: black;
+ border-width: 2px;
+ border-color: #005c28;
+ border-style: groove;
+ border-radius: 2;
+ color: white;
+ padding: 3px;]])
+
+mtw.gui.status_bars.ap.front:setStyleSheet([[background-color: QLinearGradient( x1: 1, y1: 0, x2: 0, y2: 1, stop: 0.0 #9200d6, stop: 0.24 #14001f, stop: 0.50 #100017, stop: 0.79 #0d0112, stop: 1 #0c060f);
+ border-width: 4px;
+ border-color: #005c28;
+ border-style: transparent;
+ border-radius: 2;
+ color: white;
+ padding: 0px;]])
+mtw.gui.status_bars.ap.back:setStyleSheet([[background-color: black;
+ border-width: 2px;
+ border-color: #005c28;
+ border-style: groove;
+ border-radius: 2;
+ color: white;
+ padding: 3px;]])
+
+mtw.gui.status_bars.xp.front:setStyleSheet([[background-color: QLinearGradient( x1: 1, y1: 0, x2: 0, y2: 1, stop: 0.0 #010300, stop: 0.24 #275219, stop: 0.36 #010301, stop: 0.50 #050a03, stop: 0.53 #010803, stop: 0.76 #010301, stop: 0.87 #0b0f0c, stop: 1 #02330a);
+ border-width: 4px;
+ border-color: #005c28;
+ border-style: transparent;
+ border-radius: 2;
+ color: white;
+ padding: 0px;]])
+mtw.gui.status_bars.xp.back:setStyleSheet([[background-color: black;
+ border-width: 2px;
+ border-color: #005c28;
+ border-style: groove;
+ border-radius: 2;
+ color: white;
+ padding: 3px;]])
+
+mtw.gui.location_bar:setStyleSheet([[
+ background-color = black;
+ border-color: #005c28;
+ border-style: groove;
+ border-radius: 2;
+ color: white;
+ border-width: 2px;
+]])
+
 function mtw.gui.update_location_bar()
  local cw = [[<font color="white">]]
  local cy = [[<font color="yellow">]]
