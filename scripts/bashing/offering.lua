38ce19dd680a4e89c47a596530ee8f78d09f8a87
SigmaG <sigma_github@melix.net>
2014-12-18 19:23:03 +0100

    Added offrd - offer to defile

--- scripts/bashing/offering.lua
+++ scripts/bashing/offering.lua
@@ -26,6 +26,19 @@ function mtw.offer_sanctify()
  end
 end
 
+function mtw.offer_defile()
+ if mtw.have_offerings then
+  for i in pairs(mtw.corpses) do
+   send("offer "..mtw.corpses[i].." to defile", false)
+  end
+  mtw.have_offerings = false
+  mtw.corpses = {}
+ else
+  send("ii corpse", false)
+  mtw.have_offerings = true
+ end
+end
+
 function mtw.offer_give(person)
  if mtw.have_offerings then
   for i in pairs(mtw.corpses) do
